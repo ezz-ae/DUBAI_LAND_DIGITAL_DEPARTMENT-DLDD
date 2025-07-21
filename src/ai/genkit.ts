89b67172fb498451039adea27f50d9ebdbddd08b
Mahmoud Ezz <m@ezz.ae>
2025-07-21 17:18:43 +0000

    make sure no other build issue

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,6 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
       apiVersion: 'v1beta',
     }),
   ],
